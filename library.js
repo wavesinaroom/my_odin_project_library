8620e8842126ed515486d99570becf37541bb30b
Juan Jáuregui <juan@wavesinaroom.com>
2022-07-13 15:15:40 -0500

    [Task] Refactor addBookToLibrary method

--- library.js
+++ library.js
@@ -1,22 +1,15 @@
 let myLibrary = [];
 
-const Book = {
-  author: "Author",
-  title: "Title",
-  pageNumber: 0,
-  read: false
-}
-
 function Book (author, title, pageNumber, read)
 {
-    this.author = author;
-    this.title = title;
-    this.pageNumber = pageNumber;
-    this.read = read;
+  this.author = author;
+  this.title = title;
+  this.pageNumber = pageNumber;
+  this.read = read;
 }
 
-function addBookToLibrary(book) {
-  myLibrary.push(book);
+function addBookToLibrary(author, title, pageNumber, read) {
+  myLibrary.push(Book(author, title, pageNumber, read));
 }
 
 function displayBooks(){
