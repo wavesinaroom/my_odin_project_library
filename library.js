4f7ef0ffc17758d593c940ed1397d5c78a70fc23
Juan Jáuregui <juan@wavesinaroom.com>
2022-07-13 17:31:06 -0500

    [Feature] displayBooks option now populates html with div and children p with book info

--- library.js
+++ library.js
@@ -1,3 +1,4 @@
+let documentBody = document.body;
 let myLibrary = [];
 
 function Book (author, title, pageNumber, read)
@@ -14,10 +15,26 @@ function addBookToLibrary(author, title, pageNumber, read) {
 
 function displayBooks(){
   myLibrary.forEach(function(book) {
-    console.log (book.author);
-    console.log (book.title);
-    console.log (book.pageNumber);
-    console.log (book.read);
+
+    let div = document.createElement('div');
+    let pAuthor = document.createElement('p');
+    let pTitle = document.createElement('p');
+    let pPageNumber = document.createElement('p');
+    let pRead = document.createElement('p');
+
+    pAuthor.textContent += book.author;
+    pTitle.textContent += book.title;
+    pPageNumber.textContent += book.pageNumber;
+    pRead.textContent += book.pRead;
+
+    div.id = "Book";
+
+    documentBody.appendChild(div);
+    div.appendChild(pAuthor);
+    div.appendChild(pTitle);
+    div.appendChild(pPageNumber);
+    div.appendChild(pRead);
+
   });
 }
 
