b03d62e1eaa9a0c358248c8631d089a14f8c74fb
Juan Jáuregui <juan@wavesinaroom.com>
2022-07-13 16:06:58 -0500

    [Fix] Add new keyword in addBookToLibrary function to avoid undefined exception in display books

--- library.js
+++ library.js
@@ -9,14 +9,19 @@ function Book (author, title, pageNumber, read)
 }
 
 function addBookToLibrary(author, title, pageNumber, read) {
-  myLibrary.push(Book(author, title, pageNumber, read));
+  myLibrary.push(new Book(author, title, pageNumber, read));
 }
 
 function displayBooks(){
-  myLibrary.forEach((book) => {
+  myLibrary.forEach(function(book) {
     console.log (book.author);
     console.log (book.title);
     console.log (book.pageNumber);
     console.log (book.read);
   });
 }
+
+addBookToLibrary("Gabo Márquez", "Cien años de soledad", 460, false);
+addBookToLibrary("Steven King", "The stand", 500, true);
+addBookToLibrary("Anne Rice", "The claiming of sleeping beauty", 380, false);
+displayBooks();
