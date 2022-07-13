ef2a9d8abed56491794a98cb9f712e12a27251f2
Juan Jáuregui <juan@wavesinaroom.com>
2022-07-13 14:50:35 -0500

    [Feature] Create Book object and constructor

--- library.js
+++ library.js
@@ -1,1 +1,24 @@
-alert("Yo, I'm here!");
+let myLibrary = [];
+
+const Book = {
+  author: "Author",
+  title: "Title",
+  pageNumber: 0,
+  read: false
+}
+
+function Book (author, title, pageNumber, read)
+{
+    this.author = author;
+    this.title = title;
+    this.pageNumber = pageNumber;
+    this.read = read;
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+function displayBooks(){
+
+}
